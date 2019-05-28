1e1e14a1739c682c516e5946a20b815c48cd52ce
Tom Pradat <tom.p@domraider.com>
2019-05-28 17:08:51 +0200

    :construction_worker: Add missing CI

--- tests/TranslateTest.php
+++ tests/TranslateTest.php
@@ -2,76 +2,67 @@
 
 declare(strict_types=1);
 
-use \PHPUnit\Framework\TestCase;
-use \Th3Mouk\RethinkDB\Translator\Translate;
-use \r\Connection;
-use \r\Cursor;
+namespace Th3Mouk\RethinkDB\Translator\Tests;
+
+use ArrayObject;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+use r\Connection;
+use r\Cursor;
+use stdClass;
+use Th3Mouk\RethinkDB\Translator\Translate;
 
 class TranslateTest extends TestCase
 {
     public function testCanNormalizeArrayObjectValues(): void
     {
-        $values = [
-            new ArrayObject([
-                "foo" => "bar"
-            ])
+        $values = [new ArrayObject(['foo' => 'bar']),
         ];
 
         $this->assertEquals(Translate::normalizeValues($values), [
-            ["foo" => "bar"]
+            ['foo' => 'bar'],
         ]);
     }
 
     public function testCanNormalizeDateTimeValues(): void
     {
-        $values = [
-            new DateTime('2000-05-26T13:30:20')
-        ];
+        $values = [new DateTime('2000-05-26T13:30:20')];
 
-        $this->assertEquals(Translate::normalizeValues($values), [
-            '2000-05-26T13:30:20+0200'
-        ]);
+        $this->assertEquals(Translate::normalizeValues($values), ['2000-05-26T13:30:20+0200']);
     }
 
     public function testCanNormalizeNestedValues(): void
     {
-        $values = [
-            new ArrayObject([
-                "foo" => new DateTime('2000-05-26T13:30:20')
-            ])
+        $values = [new ArrayObject([
+            'foo' => new DateTime('2000-05-26T13:30:20'),
+        ]),
         ];
 
         $this->assertEquals(Translate::normalizeValues($values), [
-            ["foo" => '2000-05-26T13:30:20+0200']
+            ['foo' => '2000-05-26T13:30:20+0200'],
         ]);
     }
 
     public function testCanTransformArrayObjectToAssociativeArray(): void
     {
-        $ao = new ArrayObject([
-            "foo" => "bar"
-        ]);
+        $ao = new ArrayObject(['foo' => 'bar']);
 
-        $this->assertEquals(Translate::arrayObjectToAssociativeArray($ao), [
-            "foo" => "bar"
-        ]);
+        $this->assertEquals(Translate::arrayObjectToAssociativeArray($ao), ['foo' => 'bar']);
     }
 
     public function testCanTransformCursorToAssociativeArray(): void
     {
         $connection = $this->createMock(Connection::class);
 
-        $data = new StdClass();
-        $data->foo = "bar";
-        $data->bar = "foo";
+        $data      = new stdClass();
+        $data->foo = 'bar';
+        $data->bar = 'foo';
 
         $cursor = new Cursor(
             $connection,
             [
-                "r" => [
-                    $data,
-                ],
-                "t" => 2
+                'r' => [$data],
+                't' => 2,
             ],
             'token',
             [],
@@ -80,9 +71,9 @@ class TranslateTest extends TestCase
 
         $this->assertEquals(Translate::cursorToAssociativeArray($cursor), [
             [
-                "foo" => "bar",
-                "bar" => "foo"
-            ]
+                'foo' => 'bar',
+                'bar' => 'foo',
+            ],
         ]);
     }
 }
