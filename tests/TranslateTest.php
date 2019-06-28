39454567b1a9cd91146049627810f6a96a077b06
Jérémy <marodon.jeremy@gmail.com>
2019-06-28 14:49:33 +0200

    :children_crossing: Improve tests readability

--- tests/TranslateTest.php
+++ tests/TranslateTest.php
@@ -4,19 +4,16 @@ declare(strict_types=1);
 
 namespace Th3Mouk\RethinkDB\Translator\Tests;
 
-use ArrayObject;
-use DateTime;
 use PHPUnit\Framework\TestCase;
 use r\Connection;
 use r\Cursor;
-use stdClass;
 use Th3Mouk\RethinkDB\Translator\Translate;
 
 class TranslateTest extends TestCase
 {
     public function testCanNormalizeArrayObjectValues(): void
     {
-        $values = [new ArrayObject(['foo' => 'bar']),
+        $values = [new \ArrayObject(['foo' => 'bar']),
         ];
 
         $this->assertEquals(Translate::normalizeValues($values), [
@@ -26,16 +23,17 @@ class TranslateTest extends TestCase
 
     public function testCanNormalizeDateTimeValues(): void
     {
-        $values = [new DateTime('2000-05-26T13:30:20')];
+        $values = [new \DateTime('2000-05-26T13:30:20+0200')];
 
         $this->assertEquals(Translate::normalizeValues($values), ['2000-05-26T13:30:20+0200']);
     }
 
     public function testCanNormalizeNestedValues(): void
     {
-        $values = [new ArrayObject([
-            'foo' => new DateTime('2000-05-26T13:30:20'),
-        ]),
+        $values = [
+            new \ArrayObject([
+                'foo' => new \DateTime('2000-05-26T13:30:20+0200'),
+            ]),
         ];
 
         $this->assertEquals(Translate::normalizeValues($values), [
@@ -45,19 +43,23 @@ class TranslateTest extends TestCase
 
     public function testCanTransformArrayObjectToAssociativeArray(): void
     {
-        $ao = new ArrayObject(['foo' => 'bar']);
+        $ao = new \ArrayObject(['foo' => 'bar']);
 
         $this->assertEquals(Translate::arrayObjectToAssociativeArray($ao), ['foo' => 'bar']);
     }
 
+    /**
+     * @throws \ReflectionException
+     */
     public function testCanTransformCursorToAssociativeArray(): void
     {
         $connection = $this->createMock(Connection::class);
 
-        $data      = new stdClass();
+        $data      = new \stdClass();
         $data->foo = 'bar';
         $data->bar = 'foo';
 
+        /** @var Connection $connection */
         $cursor = new Cursor(
             $connection,
             [
@@ -84,13 +86,13 @@ class TranslateTest extends TestCase
         $mockResult[] = new \ArrayObject([
             'domains' => ['cine.fr'],
             'id' => '46ba3de0-3210-4f7b-aac9-c5176daa0cca',
-            'status_changed_at' => new DateTime('2000-05-26T13:30:20+0200'),
+            'status_changed_at' => new \DateTime('2000-05-26T13:30:20+0200'),
         ]);
 
         $mockResult[] = new \ArrayObject([
             'domains' => ['nyxinteractive.eu', 'irond.info'],
             'id' => '4640ebcb-48f0-4948-8d5b-93cf08d4249c',
-            'status_changed_at' => new DateTime('2000-05-27T13:30:20+0200'),
+            'status_changed_at' => new \DateTime('2000-05-27T13:30:20+0200'),
         ]);
 
         $this->assertEquals(Translate::arrayOfArrayObjectToAssociativeArray($mockResult), [
